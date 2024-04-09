982bdb337207176f32a3f6bb05c87411b3f5a2b3
Amin Taher <ataher4@lsu.edu>
2024-04-08 19:21:41 -0500

    Amin push 4/8/23

--- src/screens/login/LoginPage.tsx
+++ src/screens/login/LoginPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SignIn from '../../components/LoginForm/LoginForm';
 import styles from './_loginPage.module.css'
+import { Button } from '@mui/material';
 
 const LoginPage = () => {
   return (
     <div className={styles.loginContainer}>
+        
         <SignIn />
     </div>
   )
